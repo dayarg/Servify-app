875960bf4dc300659dff9c6cebe8f2f80ad90b68
Br-alvarez <bealvarezm@academia.usbbog.edu.co>
2023-05-20 17:35:53 -0500

    creando un nuevo modelo

--- servifyappdjango/core/models.py
+++ servifyappdjango/core/models.py
@@ -14,3 +14,12 @@ class  register(models.Model):
 
     def __str__(self) :
         return self.Nombre
+    
+    class  usernew(models.Model):
+        Nombre = models.CharField(max_length=20)
+        Apellido = models.CharField(max_length=30)
+        Correo =    models.CharField(max_length=100)
+        Id_user =  models.CharField(max_length=20)
+        telefono = models.CharField(max_length=10)
+        password = models.TextField(max_length=50)
+    
--- servifyappdjango/servifyappdjango/settings.py
+++ servifyappdjango/servifyappdjango/settings.py
@@ -82,11 +82,17 @@ WSGI_APPLICATION = 'servifyappdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        "HOST":'LocalHost',
+        "PORT": 3306,
+        "USER": 'root',
+        "PASSWORD": 'password',
+        "NAME": 'servify',
+        "OPTIONS":{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
