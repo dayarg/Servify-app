ea2a8237e96a4df19e54d939c9df068c63c549f8
Br-alvarez <bealvarezm@academia.usbbog.edu.co>
2023-05-20 17:28:44 -0500

    Revert "realizando conexiones con la base de datos"
    This reverts commit 93b9be3ea319b10ffdcf990642ccf1895bac7a61.

--- servifyappdjango/core/apps.py
+++ servifyappdjango/core/apps.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.apps import AppConfig
-from .models import usuarioNuevo
+
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
-
-admin.site.register(usuarioNuevo)
