ca8544698d4f9cddd800f2c10b13e493b192f1ea
Juan Danilo Aldana Tibabisco <jdaldanat@academia.usbbog.edu.co>
2023-05-26 02:04:34 -0500

    loader created

--- servifyappdjango/react-servify/src/Routers/Routers.tsx
+++ servifyappdjango/react-servify/src/Routers/Routers.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../modules/Home/Home';
 import StartPage from '../modules/StartPage/StartPage';
@@ -10,23 +11,39 @@ import ServiceDetail from '../modules/ServiceDetail/ServiceDetail';
 import ProveedorPage from '../modules/ProveedorPage/ProveedorPage';
 import Administrator from '../modules/Administrator/Administrator';
 import LogInClient from '../modules/LogIn/LogInClient';
+import Loader from '../components/loader/loader';
 
 const Routers = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simula un tiempo de carga
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" Component={Home} />
-      <Route path="/home-supplier" Component={SupplierHome} />
-      <Route path="/login" Component={LogIn} />
-      <Route path="/login-client" Component={LogInClient} />
-      <Route path="/register" Component={Register} />
-      <Route path="/register-client" Component={RegisterClient} />
-      <Route path="/register-2" Component={RegisterDoc} />
-      <Route path="/start-page" Component={StartPage} />
-      <Route path="/servicio/:servicePath" Component={ServiceDetail} />
-      <Route path="/proveedor-page" Component={ProveedorPage} />
-      <Route path="/administrator" Component={Administrator} />
-    </Routes>
+      {isLoading ? (
+        <div className="flex justify-center items-center w-full h-full">
+          <Loader />
+        </div>
+      ) : (
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/home-supplier" Component={SupplierHome} />
+          <Route path="/login" Component={LogIn} />
+          <Route path="/login-client" Component={LogInClient} />
+          <Route path="/register" Component={Register} />
+          <Route path="/register-client" Component={RegisterClient} />
+          <Route path="/register-2" Component={RegisterDoc} />
+          <Route path="/start-page" Component={StartPage} />
+          <Route path="/servicio/:servicePath" Component={ServiceDetail} />
+          <Route path="/proveedor-page" Component={ProveedorPage} />
+          <Route path="/administrator" Component={Administrator} />
+        </Routes>
+      )}
     </BrowserRouter>
   );
 };
---
+++ servifyappdjango/react-servify/src/components/loader/loader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import loadingAnimation from '../../assets/img/media/loader.gif';
+
+const Loader: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <img src={loadingAnimation} alt="Loading" />
+    </div>
+  );
+};
+
+export default Loader;
